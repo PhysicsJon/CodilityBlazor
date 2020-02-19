b0fdc4738eb2059ff2aabdb0d699dfb24c9a12d6
PhysicsJon <j.r.alsop.18@gmail.com>
2020-02-19 15:43:02 +0000

    Fixes and code clean

--- CodilityLogin/Program.cs
+++ CodilityLogin/Program.cs
@@ -1,15 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace LoginTest
+namespace CodilityLogin
 {
     public class Program
     {
--- CodilityLogin/Startup.cs
+++ CodilityLogin/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace LoginTest
+namespace CodilityLogin
 {
     public class Startup
     {
